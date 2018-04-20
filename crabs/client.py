37dc72ed417e8d569dfe9875d3324f735041f121
tor4z <vwenjie@hotmail.com>
2018-04-21 00:06:40 +0800

    Update http client page mothod

--- crabs/client.py
+++ crabs/client.py
@@ -1,25 +1,34 @@
 from requests import Request, Session
+from crabs.page import Page
 
 class Client:
     def __init__(self):
         self._session = Session()
         self._header = {}
         self._resp = None
+        self._url = None
 
     def set_header(self, key, value):
         self._header[key] = value
 
     def get(self, url, data=None):
+        self._url = url
         req = Request('GET', url, data=data, headers=self._header)
         prepped = req.prepare()
         self._resp = self._session.send(prepped)
 
     def post(self, url, data=None):
+        self._url = url
         req = Request('POST', url, data=data, headers=self._header)
         prepped = req.prepare()
         self._resp = self._session.send(prepped)
 
     @property
-    def text(self):
+    def page(self):
         if self._resp is not None:
-            return self._resp.text
+            return Page(self._resp.text, self._url)
+        else:
+            raise NotRespExp
+
+class NotRespExp(Exception):
+    pass
