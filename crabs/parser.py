69857fd7c312b728e4918b6d39fc452dadd4b77e
tor4z <vwenjie@hotmail.com>
2018-04-21 00:10:05 +0800

    Add find all link to  HTMLParser

--- crabs/parser.py
+++ crabs/parser.py
@@ -1,20 +1,36 @@
 from bs4 import BeautifulSoup
 import re
+from crabs.url import URL
 
 class HTMLParser:
-    def __init__(self, html, parser = "html.parser"):
+    def __init__(self, page, parser = "html.parser"):
         self._parser = parser
-        self._html = html
+        self._page = page
         self._soup_ = None
+    
+    @property
+    def text(self):
+        return self._page.text
 
     @property
     def _soup(self):
         if self._soup_ is None:
-            self._soup_ = BeautifulSoup(self._html, self._parser)
+            self._soup_ = BeautifulSoup(self.text, self._parser)
         return self._soup_
 
     def find_all(self, *args, **kwargs):
         return self._soup.find_all(*args, **kwargs)
+
+    def find_all_links(self, *args, **kwargs):
+        links = self.find_all("a")
+        urls = []
+        for link in links:
+            try:
+                url = URL(link.get("href"), self._page.url)
+                urls.append(url)
+            except TypeError:
+                pass
+        return urls
 
 class StrParser:
     def __init__(self, pattern):
