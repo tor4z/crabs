49744579022b68d3a9ce4dd4935b0e270c0a23f1
tor4z <vwenjie@hotmail.com>
2018-04-21 00:09:02 +0800

    Add url dispatch

--- crabs/route.py
+++ crabs/route.py
@@ -1,7 +1,10 @@
+from crabs.handler import Handler
 import re
 
 class Route:
     _METHODS = ["GET", "POST"]
+    _HANDLERS = []
+    _PATTERNS = []
     _ROUTE = {
         "GET": [],
         "POST": []
@@ -19,20 +22,24 @@ class Route:
         self._check_method(method)
         for pt, func in cls._ROUTE[method]:
             if pt.match(url):
-                func(url)
+                func(url) # TODO
 
     @classmethod
-    def listen(cls, pattern, func, method="GET"):
-        if not callable(func):
+    def listen(cls, pattern, handler, method="GET"):
+        if not isinstance(handler, Handler):
             raise TypeError
         self._check_method(method)
         cls._ROUTE[method].append(
-            (re.compile(pattern), func)
+            (re.compile(pattern), handler)
         )
+        cls._HANDLERS.append(handler)
+        cls._PATTERNS .append(handler)
 
     @classmethod
-    def start(cls):
-        pass
+    def dispatch_url(cls, url):
+        for handler in cls._HANDLERS:
+            if handler.is_match(url):
+                return handler.new_url(url)
 
 class NotSupportMethodExp(Exception):
     pass
