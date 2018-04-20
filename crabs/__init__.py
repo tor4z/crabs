26e047dcaa503cb10fbc9ec70e5d1285ac002586
tor4z <vwenjie@hotmail.com>
2018-04-21 00:10:25 +0800

    fix mod import err

--- crabs/__init__.py
+++ crabs/__init__.py
@@ -1,5 +1,8 @@
 from crabs.crabs import Crabs
 from crabs.parser import HTMLParser, StrParser
-from crabs.data import Set, URL
+from crabs.data import Set
+from crabs.url import URL
+from crabs.page import Page
 from crabs.route import Route
 from crabs.client import Client
+from crabs.handler import Handler
