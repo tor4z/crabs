30199f78b6c3cb637c9f08af31d3b3f50afffb95
tor4z <vwenjie@hotmail.com>
2018-04-21 20:17:21 +0800

    Add utile client header

---
+++ crabs/utils.py
@@ -0,0 +1,12 @@
+ClientHeaders = {
+    "Accept":"text/html,application/xhtml+xm…plication/xml;q=0.9,*/*;q=0.8",
+    "Encoding": "gzip, deflate, br",
+    "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+    "Cache-Control": "no-cache",
+    "Connection": "keep-alive",
+    "DNT": 1,
+    "Pragma": "no-cache",
+    "Upgrade-Insecure-Requests": 1,
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0",
+    "Referer": "https://www.google.com./"
+}
