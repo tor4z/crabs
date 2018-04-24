25811b4afa81801f1f93ff2c5ccdda2aa2bf5c55
tor4z <vwenjie@hotmail.com>
2018-04-24 15:06:31 +0800

    Fix client headers

--- crabs/client/utils.py
+++ crabs/client/utils.py
@@ -1,6 +1,6 @@
 ClientHeaders = {
     "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Encoding": "gzip, deflate, br",
+    "Accept-Encoding": "gzip, deflate, br",
     "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
     "Cache-Control": "no-cache",
     "Connection": "keep-alive",
